8fb7c7319e700e69ac0ded5e29bc062a67db4600
Андрей Самойлов <samoylovanandreial@gmail.com>
2024-03-20 15:43:27 +0500

    ex1-11 fix

--- local/templates/exam1/components/bitrix/main.include/usefull-info/template.php
+++ local/templates/exam1/components/bitrix/main.include/usefull-info/template.php
@@ -13,7 +13,9 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 
-if($arResult["FILE"] <> ''){?>
+$fileContent = file_get_contents($arResult["FILE"]);
+
+if($arResult["FILE"] <> '' && strlen($fileContent) > 0){?>
     <div class="side-block side-anonse">
         <div class="title-block"><span class="i i-title01"></span>Полезная информация!</div>
         <div class="item">
